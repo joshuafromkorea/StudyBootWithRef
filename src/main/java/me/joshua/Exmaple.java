86349f6cefc4a2b6a4e918b6713f1fd6ed14895b
joshua <khandori@g.ucla.edu>
2019-05-11 18:14:42 +0900

    lecture 11

--- src/main/java/me/joshua/Exmaple.java
+++ src/main/java/me/joshua/Exmaple.java
@@ -14,6 +14,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
